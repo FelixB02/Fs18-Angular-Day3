9129191aed51c7469623ed6cbab066058843c7c9
FelixB02 <felix.blechl@chello.at>
2023-02-23 21:35:32 +0100

    Adding Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './home/home.component';
 import { CartComponent } from './cart/cart.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { products } from '../products';
 import { Iproducts } from '../Iproducts';
+import { FormGroup, FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-products',
@@ -11,6 +12,20 @@ export class ProductsComponent implements OnInit {
   products: Array<Iproducts> = products;
 
   constructor() { }
+
+  petform: any = new FormGroup({
+    picture: new FormControl('', Validators.required),
+    name: new FormControl('', Validators.required),
+    price: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    counter: new FormControl('', Validators.required),
+  });
+
+  pushPet() {
+    console.log(this.petform.value);
+    
+    this.products.push(this.petform.value)
+  }
 
   ngOnInit(): void {
   }
